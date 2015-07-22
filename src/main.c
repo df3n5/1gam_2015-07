2ae636dad4b5e17a4d3887f0a4999ad50434a997
Jonathan Frawley <jmf@9lines.org>
2015-07-23 00:16:39 +0100

    Add art assets

--- src/main.c
+++ src/main.c
@@ -24,22 +24,54 @@ int32_t menu_state(cog_state_info state) {
                 .w=1.0, .h=1.0
             }
         });
-    } else {
+    } 
+    if(cog_input_key_pressed()) {
+        cog_debugf("HI");
     }
     return S_MENU;
 }
 
 int32_t play_state(cog_state_info state) {
+    cog_debugf("PLAY STATE");
+    if(state.initial) {
+        cog_clear();
+        cog_sprite_id qid = cog_sprite_add("assets/images/quaver.png");
+        cog_sprite_set(qid, (cog_sprite) {
+            .dim=(cog_dim2) {
+                .w=0.02, .h=0.1
+            },
+            .pos=(cog_pos2) {
+                .x=-0.3, .y=0.5
+            }
+        });
+
+        cog_sprite_id cid = cog_sprite_add("assets/images/crotchet.png");
+        cog_sprite_set(cid, (cog_sprite) {
+            .dim=(cog_dim2) {
+                .w=0.02, .h=0.1
+            },
+            .pos=(cog_pos2) {
+                .x=0.3, .y=0.5
+            }
+        });
+
+        cog_sprite_id sid = cog_sprite_add("assets/images/semibreve.png");
+        cog_sprite_set(sid, (cog_sprite) {
+            .dim=(cog_dim2) {
+                .w=0.09, .h=0.1
+            },
+            .pos=(cog_pos2) {
+                .x=0.0, .y=0.0
+            }
+        });
+    } else {
+    }
     return S_LEVEL;
 }
 
 cog_state_transition transitions[] = {
     {S_MENU, COG_E_DUMMY, &menu_state},
-    {S_MENU, COG_E_KEYDOWN, &play_state},
-    {S_MENU, COG_E_MOUSELDOWN, &play_state},
-    {S_LEVEL, COG_E_DUMMY, &play_state},
-    {S_WAIT, COG_E_KEYDOWN, &play_state},
-    {S_WIN, COG_E_KEYDOWN, &menu_state}
+    {S_MENU, COG_E_KEYDOWN, &play_state}
 };
 
 void main_loop(void) {
